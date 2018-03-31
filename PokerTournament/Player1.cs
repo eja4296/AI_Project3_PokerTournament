d8c4b60f7591ede723f448bd0fd54fcf638fe77e
SPCoffey <sean@coffeyhouse.com>
2018-03-31 14:45:46 -0400

    Worked on some basic drawing strategy, commented in plans

--- PokerTournament/Player1.cs
+++ PokerTournament/Player1.cs
@@ -27,21 +27,109 @@ namespace PokerTournament
 
         public override PlayerAction Draw(Card[] hand)
         {
-            // Don't draw or discard all for testing.
-            PlayerAction action = new PlayerAction(Name, "Draw", "draw", 0);
-            int chance = rand.Next(0, 2);
+            Card high;
+            int rating = Evaluate.RateAHand(hand, out high);
+            int cardsToDraw = 0;
 
-            switch (chance)
+            switch (rating)
             {
-                case (1):
-                    // Change nothing
+                // High Card
+                case 1:
+                    if (high.Value > 10)
+                    {
+                        // We have a high card of KJQA
+                        for (int i = 0; i < hand.Length; i++)
+                        {
+                            if (hand[i].Value != high.Value)
+                            {
+                                hand[i] = null;
+                            }
+                        }
+                        cardsToDraw = 4;
+                    }
+                    else
+                    {
+                        // Discard this whole trash hand.
+                        for (int i = 0; i < hand.Length; i++)
+                        {
+                            hand[i] = null;
+                        }
+                        cardsToDraw = 5;
+                    }
                     break;
-                case (0):
-                    // discard all
-                    action = new PlayerAction(Name, "Draw", "draw", 5);
+
+                // 1 Pair
+                case 2:
+                    // Find the location of the pair
+                    // Find the highest non-pair card
+                    // If it's over 10, keep it and discard rest, if not, discard everything that isn't the pair
+                    break;
+
+                // 2 Pair
+                case 3:
+                    // Find the location of the pairs
+                    // Discard the other card
+                    break;
+
+                // 3 of a kind
+                case 4:
+                    // Find the location of the three
+                    // Find the highest non-three card
+                    // If it's over 10, keep it and discard other, if not, discard everything that isn't the three
+                    break;
+
+                // Straight
+                case 5:
+                    // Do nothing, as we don't want to discard
+                    cardsToDraw = 0;
+                    break;
+
+                // Flush
+                case 6:
+                    // Do nothing, as we don't want to discard
+                    cardsToDraw = 0;
+                    break;
+
+                // Full House
+                case 7:
+                    // Do nothing, as we don't want to discard
+                    cardsToDraw = 0;
+                    break;
+
+                // Four of a Kind
+                case 8:
+                    // Find the 4 cards
+                    // Discard the other
+                    break;
+
+                // Straight Flush
+                case 9:
+                    // Do nothing, as we don't want to discard
+                    cardsToDraw = 0;
+                    break;
+
+                // Royal Flush
+                case 10:
+                    // Do nothing, as we don't want to discard
+                    cardsToDraw = 0;
                     break;
             }
 
+            ///// Don't draw or discard all for testing.
+            ///int chance = rand.Next(0, 2);
+            ///
+            ///switch (chance)
+            ///{
+            ///    case (1):
+            ///        // Change nothing
+            ///        break;
+            ///    case (0):
+            ///        // discard all
+            ///        action = new PlayerAction(Name, "Draw", "draw", 5);
+            ///        break;
+            ///}
+
+            PlayerAction action = new PlayerAction(Name, "Draw", "draw", cardsToDraw);
             return action;
         }
     }
